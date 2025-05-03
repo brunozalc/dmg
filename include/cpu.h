ecbd48b3d2dfc2b533bd40ca4b941b6092a5814f
brunozalc <brun0zalc@hotmail.com>
2025-05-03 01:25:46 -0700

    fix: move CPU struct definition to header file

--- include/cpu.h
+++ include/cpu.h
@@ -3,8 +3,42 @@
 
 #include <stdint.h>
 
-typedef struct CPU {
-    uint16_t pc;
+typedef struct {
+    // a: accumulator; f: flags
+    union {
+        struct {
+            uint8_t f, a;
+        };
+        uint16_t af;
+    };
+
+    union {
+        struct {
+            uint8_t c, b;
+        };
+        uint16_t bc;
+    };
+
+    union {
+        struct {
+            uint8_t e, d;
+        };
+        uint16_t de;
+    };
+
+    union {
+        struct {
+            uint8_t l, h;
+        };
+        uint16_t hl;
+    };
+
+    // stack pointer and program counter are both 16 bits
+    uint16_t sp, pc;
+
+    // cpu cycle counter
+    uint64_t cycles;
+
 } CPU;
 
 #endif
