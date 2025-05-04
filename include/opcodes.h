a00fb95b7c7c3b74fbcd3344e2fdc5c4caabc7ad
brunozalc <brun0zalc@hotmail.com>
2025-05-04 11:45:10 -0700

    feat: add new opcodes and start testing w/ blargg

--- include/opcodes.h
+++ include/opcodes.h
@@ -1,15 +1,23 @@
 #ifndef OPCODES_HEADER
 #define OPCODES_HEADER
 
+#include <stdint.h>
+
 #include "cpu.h"
 
-// opfn is a function pointer that takes a CPU pointer as an argument
-// this means that we can create an array of opfns, simplifying the
-// opcode table
+// opfn is a function pointer that takes a CPU struct as an argument
+// this means that we can create an array of opfn-type functions, simplifying
+// the opcode table
 typedef void (*opfn)(CPU*);
 
 // opcode table
 // this is a 256 element array of opfn functions
 extern const opfn optable[256];
+
+// helper to advance the program counter
+static void inline advance_pc(CPU* cpu, uint8_t n) { cpu->pc += n; }
+
+// helper to advance the cycles
+static void inline advance_cycles(CPU* cpu, uint8_t n) { cpu->cycles += n; }
 
 #endif
--- lib/opcodes.c
+++ lib/opcodes.c
@@ -1,10 +1,50 @@
+/* check https://izik1.github.io/gbops/index.html */
+
 #include "opcodes.h"
 
 #include <string.h>
 
-static void op_00_nop(CPU *cpu) { cpu->cycles += 4; }
+#include "bus.h"
+#include "cpu.h"
 
+static void op_00_nop(CPU *cpu) { advance_cycles(cpu, 4); }
+
+static void op_0e_ld_c_n(CPU *cpu) {
+    cpu->c = mem_read(cpu->pc);
+    advance_pc(cpu, 1);
+    advance_cycles(cpu, 8);
+}
+
+static void op_11_ld_de_nn(CPU *cpu) {
+    uint16_t addr = mem_read16(cpu->pc);
+    cpu->de       = addr;
+    advance_pc(cpu, 2);
+    advance_cycles(cpu, 12);
+}
+
+static void op_21_ld_hl_nn(CPU *cpu) {
+    uint16_t addr = mem_read16(cpu->pc);
+    cpu->hl       = addr;
+    advance_pc(cpu, 2);
+    advance_cycles(cpu, 12);
+}
+
+static void op_47_ld_b_a(CPU *cpu) {
+    cpu->b = cpu->a;
+    advance_cycles(cpu, 4);
+}
+
+static void op_c3_jp(CPU *cpu) {
+    uint16_t addr = mem_read16(cpu->pc);
+    cpu->pc       = addr;
+    advance_cycles(cpu, 16);
+}
+
+// a table of opfn-type functions
+// table lookup has O(1) complexity and looks better than a huge switch
+// statement
 const opfn optable[256] = {
-    op_00_nop, NULL
+    [0x00] = op_00_nop,      [0x0E] = op_0e_ld_c_n, [0x11] = op_11_ld_de_nn,
+    [0x21] = op_21_ld_hl_nn, [0x47] = op_47_ld_b_a, [0xC3] = op_c3_jp,
     // TODO: implement more opcodes
 };
