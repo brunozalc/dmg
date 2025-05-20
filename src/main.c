3af55b138a34e43514aafb85de543915f3fe74e5
brunozalc <brun0zalc@hotmail.com>
2025-05-19 17:13:50 -0700

    feat: initialize `timer` in main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "cpu.h"
 #include "mmu.h"
 #include "rom.h"
+#include "timer.h"
 
 // window information
 const int DISPLAY_SCALE = 4;
@@ -13,6 +14,7 @@ const int WIDTH_PX      = 160;
 // declare the components
 CPU cpu;
 MMU mmu;
+Timer timer;
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -29,8 +31,9 @@ int main(int argc, char* argv[]) {
     const char* rom_file = argv[1];
 
     // initialize and reset components
-    cpu_init(&cpu, &mmu);
-    mmu_init(&mmu, &cpu);
+    cpu_init(&cpu, &mmu, &timer);
+    mmu_init(&mmu, &cpu, &timer);
+    timer_init(&timer, &cpu, &mmu);
 
     load_rom(&mmu, rom_file);
 
