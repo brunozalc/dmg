95cbb711ebb3bdb688061af879ac8b1300b242dc
brunozalc <brun0zalc@hotmail.com>
2025-05-19 17:15:44 -0700

    feat: add `timer` and HALT support

--- include/cpu.h
+++ include/cpu.h
@@ -8,12 +8,17 @@
 #include <string.h>
 
 #include "mmu.h"
+#include "timer.h"
 
 extern FILE *cpu_log;
 
-typedef struct MMU MMU;
+struct MMU;
+struct Timer;
 
 typedef struct CPU {
+    struct MMU *mmu;     /* pointer to the MMU */
+    struct Timer *timer; /* pointer to the timer */
+
     // a: accumulator; f: flags
     union {
         struct {
@@ -50,8 +55,8 @@ typedef struct CPU {
     uint64_t cycles;
 
     // interrupt enable register
-    int ime;     /* 0 or 1, current state (READ ONLY) */
-    int set_ime; /* 0 or 1, value to copy after one instruction */
+    int ime;       /* 0 or 1, current state (READ ONLY) */
+    int ime_delay; /* two cycle ime delay  */
 
     /* 0xFF0F, interrupt flag register
     - bit 0: v-blank
@@ -74,26 +79,41 @@ typedef struct CPU {
     uint8_t ier;
 
     /* HALT flag */
-    int halt; /* 0 or 1, current state */
+    int halt;     /* 0 or 1, current state */
+    int halt_bug; /* 0 or 1, double read bug */
 
     // last opcode executed (debugging)
     uint8_t last_opcode;
 
-    MMU *mmu; /* pointer to the MMU */
-
 } CPU;
 
-void cpu_init(CPU *cpu, MMU *mmu);
+void cpu_init(CPU *cpu, MMU *mmu, Timer *timer);
 void cpu_step(CPU *cpu);
 
-/* logs cpu state after execution in the format:
-- saves the line in a file called cpu.log
-*/
-inline void log_cpu_state(CPU *cpu);
+/* function to log CPU errors with useful information */
+// static inline void log_cpu_error(CPU *cpu, const char *format, ...) {
+//     va_list args;
+//     va_start(args, format);
 
-/* function to log an error
-- prints information about the CPU state when the error occurred
-*/
-void log_cpu_error(CPU *cpu, const char *format, ...);
+//     fprintf(stderr, "\n=== error ===\n");
+//     fprintf(stderr, "pc: 0x%04X\n", cpu->pc);
+//     fprintf(stderr, "opcode: 0x%02X\n", cpu->last_opcode);
+//     fprintf(stderr, "error: ");
+//     vfprintf(stderr, format, args);
+//     fprintf(stderr, "\n");
+
+//     // print CPU state
+//     fprintf(stderr, "\nCPU state:\n");
+//     fprintf(stderr, "A: 0x%02X  F: 0x%02X\n", cpu->a, cpu->f);
+//     fprintf(stderr, "B: 0x%02X  C: 0x%02X\n", cpu->b, cpu->c);
+//     fprintf(stderr, "D: 0x%02X  E: 0x%02X\n", cpu->d, cpu->e);
+//     fprintf(stderr, "H: 0x%02X  L: 0x%02X\n", cpu->h, cpu->l);
+//     fprintf(stderr, "sp: 0x%04X\n", cpu->sp);
+//     fprintf(stderr, "cycles: %llu\n", cpu->cycles);
+//     fprintf(stderr, "===================\n\n");
+
+//     va_end(args);
+//     exit(1);
+// }
 
 #endif
